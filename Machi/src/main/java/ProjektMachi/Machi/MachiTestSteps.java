41e84d7f32a96ebc8741a6672e69c71fa73682b4
sesec <sesec@LAPTOP-CL0U0HP0>
2018-08-23 11:32:08 +0200

    sdjfvsdaf

--- Machi/src/main/java/ProjektMachi/Machi/MachiTestSteps.java
+++ Machi/src/main/java/ProjektMachi/Machi/MachiTestSteps.java
@@ -1,5 +1,8 @@
 package ProjektMachi.Machi;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -41,6 +44,34 @@ public class MachiTestSteps {
 	   }
 	}
 	
-	//Userstory2: Select activity date		
+	//Userstory2: Select activity date	
+	
+	//User story1
+	
+	@When("^I enter the \"([^\"]*)\" of a Sport Hall$")
+	public void i_enter_the_of_a_Sport_Hall(String arg1) throws Throwable {
+	    
+		//WebElement element = webDriver.findElement(By.xpath("//a[contains(text(),'Anläggningar')]"));
+		
+	  //*[@id="navbar-collapse"]/ul[1]/li[2]/a
+	    //#navbar-collapse > ul.nav.navbar-nav.navbar-left > li:nth-child(2) > a
+	  //*[@id="navbar-collapse"]/ul[1]/li[3]/a      xpath- för matchning
+	  //a[contains(text(),'Anläggningar')]
+	  //*[@id="q"]
+	}
+	
+	@When("^As I click the name of the Sport Hall$")
+	public void as_I_click_the_name_of_the_Sport_Hall() throws Throwable {
+	    
+	    throw new PendingException();
+	}
+
+	@Then("^Can choose the clicked Sport Hall for booking$")
+	public void can_choose_the_clicked_Sport_Hall_for_booking() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
 
 }
